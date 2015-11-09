f6b3777a277898f12c9fa3e4fecffb25b401acd1
Jacob Chan <jkcchan@uwaterloo.ca>
2015-11-08 17:50:04 -0700

    testing auth

--- auth.js
+++ auth.js
@@ -1,9 +1,15 @@
-var url="";
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-	url = tabs[0].url;
-});
+var url=window.location.href;
 url = url.split("?");
 var state = url[1].split("&")[0];
 var code = url[1].split("&")[1];
-document.getElementById('state').innerHTML(state);
-document.getElementById('code').innerHTML(code);
+state = state.split("=")[1];
+code = code.split("=")[1];
+console.log(state,code);
+if(state&&code){
+	$.ajax({
+		type:'POST',
+		url:"https://www.reddit.com/api/v1/access_token",
+		data:"grant_type=authorization_code&code="+code+"&redirect_uri=http://jacobchan.ca/Quick-Share",
+
+	})
+}
